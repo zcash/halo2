d70c8cc0d739aaf3a4853f4604a6bce4a739df35
therealyingtong <yingtong@electriccoin.co>
2020-09-16 00:44:56 +0800

    Add benchmarks for shared double-and-add

--- src/poly/commitment.rs
+++ src/poly/commitment.rs
@@ -261,6 +261,11 @@ impl<C: CurveAffine> Params<C> {
             other_bases,
         }
     }
+
+    /// Getter for g generators
+    pub fn get_g(&self) -> Vec<C> {
+        self.g.clone()
+    }
 }
 
 /// A guard returned by the verifier
