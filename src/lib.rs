7905a0c80a7baf2fcd58abe72b98b9d097cdec2a
Jack Grigg <jack@electriccoin.co>
2020-10-20 22:44:33 +0100

    Update crate attributes

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 //! # pollard
 
-#![deny(intra_doc_link_resolution_failure)]
+#![cfg_attr(docsrs, feature(doc_cfg))]
+// Catch documentation errors caused by code changes.
+#![deny(broken_intra_doc_links)]
 #![deny(missing_debug_implementations)]
 #![deny(missing_docs)]
 #![deny(unsafe_code)]
