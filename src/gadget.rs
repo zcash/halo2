f24b60b5b0513e42615bf8cdbca6e7185e179fb1
Jack Grigg <jack@electriccoin.co>
2021-01-08 01:55:10 +0000

    Add a placeholder module for gadgets

---
+++ src/gadget.rs
@@ -0,0 +1,1 @@
+//! Self-contained circuit implementations of various primitives.
--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,7 @@
 
 pub mod arithmetic;
 pub mod circuit;
+pub mod gadget;
 pub mod pasta;
 pub mod plonk;
 pub mod poly;
