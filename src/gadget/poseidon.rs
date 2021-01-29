b832b1439cdfd9853dc797e11ac3129eb4aa1e50
Jack Grigg <jack@electriccoin.co>
2021-01-29 14:26:52 +0000

    [WIP] Poseidon instructions

---
+++ src/gadget/poseidon.rs
@@ -0,0 +1,20 @@
+//! Gadget and chips for the Poseidon algebraic hash function.
+
+use std::fmt;
+
+use crate::{
+    circuit::{Chip, Layouter},
+    plonk::Error,
+};
+
+/// The set of circuit instructions required to use the [`Poseidon`] gadget.
+pub trait PoseidonInstructions: Chip {
+    /// Variable representing the state over which the Poseidon permutation operates.
+    type State: fmt::Debug;
+
+    /// Applies the Poseidon permutation to the given state.
+    fn permute(
+        layouter: &mut impl Layouter<Self>,
+        initial_state: &Self::State,
+    ) -> Result<Self::State, Error>;
+}
