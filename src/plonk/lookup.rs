1d3d7261c9041a80b34c405a627add6d7120ed2a
therealyingtong <yingtong@z.cash>
2021-02-12 09:40:53 +0800

    Calculate required degree of lookup

--- src/plonk/lookup.rs
+++ src/plonk/lookup.rs
@@ -25,7 +25,7 @@ impl<F: Field> Argument<F> {
         // degree 2:
         // l_0(X) * (1 - z'(X)) = 0
         //
-        // degree 3:
+        // degree (1 + input_degree + table_degree):
         // z'(X) (a'(X) + \beta) (s'(X) + \gamma)
         // - z'(\omega^{-1} X) (\theta^{m-1} a_0(X) + ... + a_{m-1}(X) + \beta) (\theta^{m-1} s_0(X) + ... + s_{m-1}(X) + \gamma)
         //
@@ -34,6 +34,15 @@ impl<F: Field> Argument<F> {
         //
         // degree 2:
         // (a′(X)−s′(X))⋅(a′(X)−a′(\omega{-1} X)) = 0
-        3
+        let mut input_degree = 1;
+        for expr in self.input_columns.iter() {
+            input_degree = std::cmp::max(input_degree, expr.degree());
+        }
+        let mut table_degree = 1;
+        for expr in self.table_columns.iter() {
+            table_degree = std::cmp::max(table_degree, expr.degree());
+        }
+
+        1 + input_degree + table_degree
     }
 }
