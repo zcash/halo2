d3519ee72073161ce825d8936963645e34fb54e1
therealyingtong <yingtong@z.cash>
2021-02-14 23:25:49 +0800

    Add Sinsemilla test

--- src/gadget/sinsemilla.rs
+++ src/gadget/sinsemilla.rs
@@ -42,3 +42,98 @@ pub trait SinsemillaInstructions<F: FieldExt>: Chip<Field = F> {
         message: Self::Message,
     ) -> Result<Self::HashOutput, Error>;
 }
+
+#[test]
+fn test_sinsemilla() {
+    use crate::arithmetic::{CurveAffine, FieldExt};
+    use crate::circuit::layouter::SingleChip;
+    use crate::pasta::{EpAffine, EqAffine};
+    use crate::plonk::*;
+    use crate::poly::commitment::Params;
+    use crate::transcript::{Blake2bRead, Blake2bWrite};
+
+    use std::marker::PhantomData;
+
+    /// This represents an advice column at a certain row in the ConstraintSystem
+    #[derive(Copy, Clone, Debug)]
+    pub struct Variable(Column<Advice>, usize);
+
+    struct MyCircuit<C: CurveAffine> {
+        message: Vec<bool>,
+        _marker_c: PhantomData<C>,
+    }
+
+    impl<'a, C: Chip, CS: Assignment<C::Field>> SingleChip<'a, C, CS> {
+        fn load_new<Cu: CurveAffine<Base = C::Field>>(
+            cs: &'a mut CS,
+            config: C::Config,
+        ) -> Result<Self, Error> {
+            let mut res = SingleChip::new(cs, config);
+
+            C::load(&mut res)?;
+
+            Ok(res)
+        }
+    }
+
+    impl<F: FieldExt, C: CurveAffine<Base = F>> Circuit<F> for MyCircuit<C> {
+        type Config = SinsemillaConfig;
+
+        fn configure(meta: &mut ConstraintSystem<F>) -> Self::Config {
+            let columns = SinsemillaColumns::new(
+                meta.fixed_column(),
+                meta.advice_column(),
+                meta.advice_column(),
+                meta.advice_column(),
+                meta.advice_column(),
+                meta.advice_column(),
+            );
+
+            SinsemillaChip::<C>::configure(meta, 11, 2, columns)
+        }
+
+        fn synthesize(
+            &self,
+            cs: &mut impl Assignment<F>,
+            config: Self::Config,
+        ) -> Result<(), Error> {
+            let mut layouter = SingleChip::load_new::<C>(cs, config)?;
+
+            SinsemillaChip::<C>::hash(&mut layouter, self.message.clone())?;
+
+            Ok(())
+        }
+    }
+
+    // Initialize the polynomial commitment parameters
+    let k = 11;
+    let params: Params<EqAffine> = Params::new(k);
+    let empty_circuit: MyCircuit<EpAffine> = MyCircuit {
+        message: Vec::new(),
+        _marker_c: PhantomData,
+    };
+
+    // Initialize the proving key
+    let vk = keygen_vk(&params, &empty_circuit).expect("keygen_vk should not fail");
+    let pk = keygen_pk(&params, vk, &empty_circuit).expect("keygen_pk should not fail");
+
+    let circuit: MyCircuit<EpAffine> = MyCircuit {
+        // 101101101101
+        message: vec![
+            true, false, true, true, false, true, true, false, true, true, false, false,
+        ],
+        _marker_c: PhantomData,
+    };
+
+    // Create a proof
+    let mut transcript = Blake2bWrite::init(vec![]);
+    create_proof(&params, &pk, &[circuit], &[&[]], &mut transcript)
+        .expect("proof generation should not fail");
+    let proof: Vec<u8> = transcript.finalize();
+
+    let msm = params.empty_msm();
+    let mut transcript = Blake2bRead::init(&proof[..]);
+    let guard = verify_proof(&params, pk.get_vk(), msm, &[&[]], &mut transcript).unwrap();
+    let msm = guard.clone().use_challenges();
+    assert!(msm.eval());
+}
