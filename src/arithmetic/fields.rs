df481ea760b04ca97ff811a9ab34949928e8275c
honeywest <honeywest@foxmail.com>
2021-02-27 21:45:04 +0800

    optimize fields sbb

--- src/arithmetic/fields.rs
+++ src/arithmetic/fields.rs
@@ -365,8 +365,8 @@ pub(crate) const fn adc(a: u64, b: u64, carry: u64) -> (u64, u64) {
 /// Compute a - (b + borrow), returning the result and the new borrow.
 #[inline(always)]
 pub(crate) const fn sbb(a: u64, b: u64, borrow: u64) -> (u64, u64) {
-    let ret = (a as u128).wrapping_sub((b as u128) + ((borrow >> 63) as u128));
-    (ret as u64, (ret >> 64) as u64)
+    let ret = (a as u128).wrapping_sub((b as u128) + (borrow as u128));
+    (ret as u64, (ret >> 127) as u64)
 }
 
 /// Compute a + (b * c) + carry, returning the result and the new carry over.
