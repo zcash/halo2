5772c71a898ce98ee037c0a9645b0da92ae396f1
Jack Grigg <jack@electriccoin.co>
2021-03-06 00:57:30 +0000

    Add doctest example to orchard::Address that exercises key derivation

--- src/address.rs
+++ src/address.rs
@@ -3,6 +3,15 @@ use halo2::pasta::pallas;
 use crate::keys::Diversifier;
 
 /// A shielded payment address.
+///
+/// # Examples
+///
+/// ```
+/// use orchard::keys::{SpendingKey, FullViewingKey};
+///
+/// let sk = SpendingKey::from_bytes([7; 32]).unwrap();
+/// let address = FullViewingKey::from(&sk).default_address();
+/// ```
 #[derive(Debug)]
 pub struct Address {
     d: Diversifier,
