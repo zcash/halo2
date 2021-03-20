b8f02c3b32f27a46dcca88723ac7516d6422bf65
Jack Grigg <thestr4d@gmail.com>
2021-03-20 18:31:09 +1300

    Temporarily allow dead code
    This will make the lints more useful while we are implementing the
    Orchard protocol.

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,8 @@
 //! address, which is also shielded).
 
 #![cfg_attr(docsrs, feature(doc_cfg))]
+// Temporary until we have more of the crate implemented.
+#![allow(dead_code)]
 // Catch documentation errors caused by code changes.
 #![deny(broken_intra_doc_links)]
 #![deny(missing_debug_implementations)]
