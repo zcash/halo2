d58fed183c40f2646b3c3313b498c97ffb5f0522
Jack Grigg <jack@electriccoin.co>
2021-05-18 20:07:05 +0100

    Fix warnings due to clippy::unknown_clippy_lints being renamed
    We can remove these workarounds once our MSRV is 1.51+.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
 //! # halo2
 
 #![cfg_attr(docsrs, feature(doc_cfg))]
+// Build without warnings on stable 1.51 and later.
 #![allow(unknown_lints)]
+// Disable old lint warnings until our MSRV is at least 1.51.
+#![allow(renamed_and_removed_lints)]
+// Use the old lint name to build without warnings until our MSRV is at least 1.51.
+#![allow(clippy::unknown_clippy_lints)]
+// The actual lints we want to disable.
 #![allow(
     clippy::op_ref,
     clippy::assign_op_pattern,
@@ -9,8 +15,7 @@
     clippy::suspicious_arithmetic_impl,
     clippy::many_single_char_names,
     clippy::same_item_push,
-    clippy::upper_case_acronyms,
-    clippy::unknown_clippy_lints
+    clippy::upper_case_acronyms
 )]
 #![deny(broken_intra_doc_links)]
 #![deny(missing_debug_implementations)]
