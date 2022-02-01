628bdb34070afdd6912a9638c5fccfbbfa824913
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-01-31 20:10:19 -0500

    refactor

--- flake.nix
+++ flake.nix
@@ -1,15 +1,14 @@
 { inputs =
-    { make-shell.url = "github:ursi/nix-make-shell/1";
-      nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    { nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
       utils.url = "github:ursi/flake-utils/6";
     };
 
   outputs = { utils, ... }@inputs:
     utils.for-default-systems
-      ({ make-shell, pkgs, ... }:
+      ({ pkgs, ... }:
          { devShell =
              pkgs.mkShell
-               { nativeBuildInputs =
+               { buildInputs =
                    with pkgs;
                    [ cargo
                      gcc
