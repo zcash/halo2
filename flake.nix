117732b8e310b4fe349ddcb9a87627cc2c832fe2
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-01-31 20:17:24 -0500

    add cargo2nix

--- flake.nix
+++ flake.nix
@@ -1,16 +1,18 @@
 { inputs =
-    { nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    { cargo2nix.url = "github:cargo2nix/cargo2nix";
+      nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
       utils.url = "github:ursi/flake-utils/6";
     };
 
   outputs = { utils, ... }@inputs:
     utils.for-default-systems
-      ({ pkgs, ... }:
+      ({ cargo2nix, pkgs, ... }:
          { devShell =
              pkgs.mkShell
                { buildInputs =
                    with pkgs;
                    [ cargo
+                     cargo2nix
                      gcc
                      rustc
                      cmake
