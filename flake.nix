9acdd994130110dbc1e91df1f0e9630938cc65b1
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-01-31 20:43:15 -0500

    build halo 2 with nix

--- flake.nix
+++ flake.nix
@@ -1,13 +1,51 @@
 { inputs =
     { cargo2nix.url = "github:cargo2nix/cargo2nix";
       nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+      rust-overlay.url = "github:oxalica/rust-overlay";
       utils.url = "github:ursi/flake-utils/6";
     };
 
-  outputs = { utils, ... }@inputs:
+  outputs = { cargo2nix, nixpkgs, rust-overlay, utils, ... }@inputs:
     utils.for-default-systems
-      ({ cargo2nix, pkgs, ... }:
-         { devShell =
+      ({ system, ... }:
+         let
+           pkgs =
+             import nixpkgs
+               { inherit system;
+
+                 overlays =
+                   [ (import "${cargo2nix}/overlay")
+                     rust-overlay.overlay
+                   ];
+               };
+         in
+         { defaultPackage =
+             let
+               rustPkgs =
+                 pkgs.rustBuilder.makePackageSet'
+                   { rustChannel = "1.56.1";
+                     packageFun = import ./Cargo.nix;
+                     packageOverrides =
+                       let
+                         expat-sys = pkgs.rustBuilder.rustLib.makeOverride {
+                           name = "expat-sys";
+                           overrideAttrs = drv: {
+                             propagatedBuildInputs = drv.propagatedBuildInputs or [ ] ++ [ pkgs.expat ];
+                           };
+                         };
+                         freetype-sys = pkgs.rustBuilder.rustLib.makeOverride {
+                           name = "freetype-sys";
+                           overrideAttrs = drv: {
+                             propagatedBuildInputs = drv.propagatedBuildInputs or [ ] ++ [ pkgs.freetype ];
+                           };
+                         };
+                      in
+                      pkgs: pkgs.rustBuilder.overrides.all ++ [ expat-sys freetype-sys ];
+                   };
+             in
+             (rustPkgs.workspace.halo2_proofs {});
+
+           devShell =
              pkgs.mkShell
                { buildInputs =
                    with pkgs;
