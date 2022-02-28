92d8ae5def974d8fadafe1e564a6ad8993e3c99c
therealyingtong <yingtong@z.cash>
2022-02-28 22:58:45 +0800

    endoscale::chip::alg_2: Implement compute_endoscalar

--- halo2_gadgets/src/endoscale/chip/alg_2.rs
+++ halo2_gadgets/src/endoscale/chip/alg_2.rs
@@ -1,8 +1,12 @@
-use ff::PrimeFieldBits;
+use ff::{Field, PrimeFieldBits};
 use halo2_proofs::{
     arithmetic::{CurveAffine, FieldExt},
     circuit::{AssignedCell, Layouter, Value},
-    plonk::{Advice, Assigned, Column, ConstraintSystem, Error, Instance, Selector, TableColumn},
+    plonk::{
+        Advice, Assigned, Column, ConstraintSystem, Constraints, Error, Expression, Instance,
+        Selector, TableColumn,
+    },
+    poly::Rotation,
 };
 
 use crate::{
@@ -18,6 +22,16 @@ use std::marker::PhantomData;
 pub struct Bitstring<F: FieldExt + PrimeFieldBits, const K: usize> {
     running_sum: RunningSum<F, K>,
     pad_len: usize,
+}
+
+impl<F: FieldExt + PrimeFieldBits, const K: usize> Bitstring<F, K> {
+    #[cfg(test)]
+    fn bitstring(&self) -> Value<Vec<bool>> {
+        let num_bits = self.running_sum.num_bits();
+        self.running_sum.zs()[0]
+            .value()
+            .map(|v| v.to_le_bits().iter().by_vals().take(num_bits).collect())
+    }
 }
 
 /// Configuration for endoscalar table.
@@ -110,7 +124,7 @@ where
 
         let table = TableConfig::configure(meta);
 
-        Self {
+        let config = Self {
             q_lookup: meta.complex_selector(),
             q_endoscale_scalar: meta.selector(),
             endoscalars,
@@ -118,7 +132,37 @@ where
             acc,
             running_sum_chunks,
             table,
-        }
+        };
+
+        meta.create_gate("Endoscale scalar with lookup", |meta| {
+            let q_endoscale_scalar = meta.query_selector(config.q_endoscale_scalar);
+            let endo = meta.query_advice(config.endoscalars_copy, Rotation::cur());
+            let acc = meta.query_advice(config.acc, Rotation::cur());
+            let next_acc = meta.query_advice(config.acc, Rotation::next());
+
+            // Check that next_acc = acc * 2^{K/2} + endo
+            let expected_next_acc = acc * C::Base::from(1 << (K / 2)) + endo;
+
+            Constraints::with_selector(q_endoscale_scalar, [next_acc - expected_next_acc])
+        });
+
+        meta.lookup(|meta| {
+            let q_lookup = meta.query_selector(config.q_lookup);
+            let word = config.running_sum_chunks.window_expr_be(meta);
+            let endo = meta.query_advice(config.endoscalars_copy, Rotation::cur());
+
+            let neg_q_lookup = Expression::Constant(C::Base::one()) - q_lookup.clone();
+            let default_endo = {
+                let val = compute_endoscalar_with_acc(Some(C::Base::zero()), &[false; K]);
+                Expression::Constant(val)
+            };
+
+            let endo_expr = q_lookup.clone() * endo + neg_q_lookup * default_endo;
+
+            vec![(q_lookup * word, table.bits), (endo_expr, table.endoscalar)]
+        });
+
+        config
     }
 
     pub(super) fn witness_bitstring(
@@ -165,10 +209,86 @@ where
 
     pub(super) fn compute_endoscalar(
         &self,
-        mut _layouter: &mut impl Layouter<C::Base>,
-        _bitstring: &Bitstring<C::Base, K>,
+        layouter: &mut impl Layouter<C::Base>,
+        bitstring: &Bitstring<C::Base, K>,
     ) -> Result<AssignedCell<Assigned<C::Base>, C::Base>, Error> {
-        todo!()
+        // TODO: account for padding bits
+        let num_bits = bitstring.running_sum.num_bits();
+        // num_bits must be an even number not greater than MAX_BITSTRING_LENGTH.
+        assert!(num_bits <= MAX_BITSTRING_LENGTH);
+
+        layouter.assign_region(
+            || "Endoscale scalar using bitstring (lookup optimisation)",
+            |mut region| {
+                let offset = 0;
+                // The endoscalar is initialised to 2 * (ζ + 1).
+                let mut acc = {
+                    let init = (C::Base::ZETA + C::Base::one()).double();
+                    region.assign_advice_from_constant(
+                        || "initialise acc",
+                        self.acc,
+                        offset,
+                        init,
+                    )?
+                };
+
+                // Copy the running sum
+                let running_sum_len = bitstring.running_sum.zs().len();
+                for (idx, z) in bitstring.running_sum.zs().iter().rev().enumerate() {
+                    z.copy_advice(
+                        || format!("z[{:?}]", running_sum_len - idx),
+                        &mut region,
+                        self.running_sum_chunks.z(),
+                        offset + idx,
+                    )?;
+                }
+
+                // For each chunk, lookup the (chunk, endoscalar) pair and add
+                // it to the accumulator.
+                for (idx, chunk) in bitstring.running_sum.windows().iter().rev().enumerate() {
+                    self.q_lookup.enable(&mut region, offset + idx)?;
+                    self.q_endoscale_scalar.enable(&mut region, offset + idx)?;
+
+                    let endoscalar = chunk.map(|c| {
+                        compute_endoscalar_with_acc(
+                            Some(C::Base::zero()),
+                            &c.to_le_bits().iter().by_vals().take(K).collect::<Vec<_>>(),
+                        )
+                    });
+                    // Witness endoscalar.
+                    region.assign_advice(
+                        || format!("Endoscalar for chunk {}", running_sum_len - idx),
+                        self.endoscalars_copy,
+                        offset + idx,
+                        || endoscalar,
+                    )?;
+
+                    // Bitshift the accumulator by {K / 2} and add to endoscalar.
+                    let acc_val = acc.value().zip(endoscalar).map(|(&acc, endo)| {
+                        let two_pow_k_div2 = C::Base::from(1 << (K / 2));
+                        acc * two_pow_k_div2 + endo
+                    });
+                    acc = region.assign_advice(
+                        || format!("Endoscalar for chunk {}", running_sum_len - idx),
+                        self.acc,
+                        offset + idx + 1,
+                        || acc_val,
+                    )?;
+                }
+
+                #[cfg(test)]
+                {
+                    use crate::endoscale::util::compute_endoscalar;
+                    let bitstring = bitstring.bitstring();
+                    let expected_acc: Value<C::Base> = bitstring.map(|b| compute_endoscalar(&b));
+                    acc.value()
+                        .zip(expected_acc)
+                        .map(|(&acc, expected_acc)| assert_eq!(acc, expected_acc));
+                }
+
+                Ok(acc.into())
+            },
+        )
     }
 
     pub(super) fn constrain_bitstring(
