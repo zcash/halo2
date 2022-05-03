081066971842e453979f87cfab045bd48d872214
Zhang Zhuo <mycinbrin@gmail.com>
2022-05-03 09:27:22 +0800

    Update commitment.rs

--- halo2_proofs/src/poly/commitment.rs
+++ halo2_proofs/src/poly/commitment.rs
@@ -86,8 +86,9 @@ impl<C: CurveAffine> Params<C> {
         for _ in k..E::Scalar::S {
             root = root.square();
         }
+        let n_inv = E::Scalar::from(n).invert().expect("inversion should be ok for n = 1<<k");
         let multiplier =
-            (s.pow_vartime(&[n as u64]) - E::Scalar::one()) * E::Scalar::from(n).invert().unwrap();
+            (s.pow_vartime(&[n as u64]) - E::Scalar::one()) * n_inv;
         parallelize(&mut g_lagrange_projective, |g, start| {
             for (idx, g) in g.iter_mut().enumerate() {
                 let offset = start + idx;
