473183ed4fc63254b557d5a9654efe17449929e7
NoCtrlZ <phantomofrotten@gmail.com>
2022-05-09 16:22:20 +0900

    refactor and optimize fft

--- halo2_proofs/src/arithmetic.rs
+++ halo2_proofs/src/arithmetic.rs
@@ -166,18 +166,14 @@ pub fn best_multiexp<C: CurveAffine>(coeffs: &[C::Scalar], bases: &[C]) -> C::Cu
 /// distinct powers of $\omega$. This transformation is invertible by providing
 /// $\omega^{-1}$ in place of $\omega$ and dividing each resulting field element
 /// by $n$.
+///
+/// This will use multithreading if beneficial.
 pub fn best_fft<G: Group>(a: &mut [G], omega: G::Scalar, log_n: u32) {
     let n = a.len() as usize;
     assert_eq!(n, 1 << log_n);
 
-    // sort by bit reverse
-    let diff = 64 - log_n;
-    for i in 0..n as u64 {
-        let ri = i.reverse_bits() >> diff;
-        if i < ri {
-            a.swap(ri as usize, i as usize);
-        }
-    }
+    // swap bit reverse index
+    swap_bit_reverse(a, n, log_n);
 
     // precompute twiddle factors
     let twiddles: Vec<_> = (0..(n / 2) as usize)
@@ -191,7 +187,9 @@ pub fn best_fft<G: Group>(a: &mut [G], omega: G::Scalar, log_n: u32) {
     if log_n <= log_threads() {
         serial_butterfly_arithmetic(a, n, log_n, &twiddles)
     } else {
-        parallel_butterfly_arithmetic(a, n, 1, &twiddles)
+        let (left, right) = a.split_at_mut(n / 2);
+        parallel_butterfly_arithmetic(left, right, n / 2, 2, &twiddles);
+        two_argument_butterfly_arithmetic(left, right, 1, &twiddles)
     }
 }
 
@@ -207,59 +205,76 @@ fn serial_butterfly_arithmetic<G: Group>(
     for _ in 0..log_n {
         a.chunks_mut(chunk).for_each(|coeffs| {
             let (left, right) = coeffs.split_at_mut(chunk / 2);
-            butterfly_arithmetic(left, right, twiddle_chunk, twiddles)
+            two_argument_butterfly_arithmetic(left, right, twiddle_chunk, twiddles)
         });
         chunk *= 2;
         twiddle_chunk /= 2;
     }
 }
 
-/// This performs parallel butterfly arithmetic
-pub fn parallel_butterfly_arithmetic<G: Group>(
-    a: &mut [G],
+/// This performs recursive butterfly arithmetic
+fn parallel_butterfly_arithmetic<G: Group>(
+    left: &mut [G],
+    right: &mut [G],
     n: usize,
     twiddle_chunk: usize,
     twiddles: &[G::Scalar],
 ) {
-    if n == 2 {
-        let t = a[1];
-        a[1] = a[0];
-        a[0].group_add(&t);
-        a[1].group_sub(&t);
+    if n == 4 {
+        self_swap_arithmetic(left, right, 0, 1);
+        self_swap_arithmetic(left, right, 2, 3);
+        self_swap_arithmetic(left, right, 0, 2);
+        self_swap_arithmetic_with_twiddle(left, right, &twiddles[twiddle_chunk], 1, 3);
     } else {
-        let (left, right) = a.split_at_mut(n / 2);
+        let (a, b) = left.split_at_mut(n / 2);
+        let (c, d) = right.split_at_mut(n / 2);
         rayon::join(
-            || parallel_butterfly_arithmetic(left, n / 2, twiddle_chunk * 2, twiddles),
-            || parallel_butterfly_arithmetic(right, n / 2, twiddle_chunk * 2, twiddles),
+            || parallel_butterfly_arithmetic(a, b, n / 2, twiddle_chunk * 2, twiddles),
+            || parallel_butterfly_arithmetic(c, d, n / 2, twiddle_chunk * 2, twiddles),
         );
-
-        butterfly_arithmetic(left, right, twiddle_chunk, twiddles)
+        four_argument_butterfly_arithmetic(a, b, c, d, twiddle_chunk, twiddles);
     }
 }
 
-/// This performs butterfly arithmetic
-fn butterfly_arithmetic<G: Group>(
+/// This performs butterfly arithmetic with two `G` array
+fn two_argument_butterfly_arithmetic<G: Group>(
     left: &mut [G],
     right: &mut [G],
     twiddle_chunk: usize,
     twiddles: &[G::Scalar],
 ) {
     // case when twiddle factor is one
-    let t = right[0];
-    right[0] = left[0];
-    left[0].group_add(&t);
-    right[0].group_sub(&t);
+    swap_arithmetic(&mut left[0], &mut right[0]);
 
     left.iter_mut()
         .zip(right.iter_mut())
         .enumerate()
         .skip(1)
-        .for_each(|(i, (a, b))| {
-            let mut t = *b;
-            t.group_scale(&twiddles[i * twiddle_chunk]);
-            *b = *a;
-            a.group_add(&t);
-            b.group_sub(&t);
+        .for_each(|(i, (a, b))| swap_arithmetic_with_twiddle(a, b, &twiddles[i * twiddle_chunk]));
+}
+
+/// This performs butterfly arithmetic with four `G` array
+fn four_argument_butterfly_arithmetic<G: Group>(
+    a: &mut [G],
+    b: &mut [G],
+    c: &mut [G],
+    d: &mut [G],
+    twiddle_chunk: usize,
+    twiddles: &[G::Scalar],
+) {
+    // case when twiddle factor is one
+    swap_arithmetic(&mut a[0], &mut b[0]);
+    swap_arithmetic(&mut c[0], &mut d[0]);
+
+    a.iter_mut()
+        .zip(b.iter_mut())
+        .zip(c.iter_mut())
+        .zip(d.iter_mut())
+        .enumerate()
+        .skip(1)
+        .for_each(|(i, (((a, b), c), d))| {
+            swap_arithmetic_with_twiddle(a, b, &twiddles[i * twiddle_chunk]);
+            swap_arithmetic_with_twiddle(c, d, &twiddles[i * twiddle_chunk]);
         });
 }
 
@@ -388,9 +403,71 @@ pub fn lagrange_interpolate<F: FieldExt>(points: &[F], evals: &[F]) -> Vec<F> {
     }
 }
 
-use crate::pasta::Fp;
+fn swap_bit_reverse<G: Group>(a: &mut [G], n: usize, log_n: u32) {
+    // sort by bit reverse
+    let diff = 64 - log_n;
+    for i in 0..n as u64 {
+        let ri = i.reverse_bits() >> diff;
+        if i < ri {
+            a.swap(ri as usize, i as usize);
+        }
+    }
+}
+
+/// This performs permutation and butterfly arithmetic without twiddle for `G`
+fn swap_arithmetic<G: Group>(a: &mut G, b: &mut G) {
+    let t = *b;
+    *b = *a;
+    a.group_add(&t);
+    b.group_sub(&t);
+}
+
+/// This performs permutation and butterfly arithmetic twiddle for `G`
+fn swap_arithmetic_with_twiddle<G: Group>(a: &mut G, b: &mut G, twiddle: &G::Scalar) {
+    let mut t = *b;
+    t.group_scale(twiddle);
+    *b = *a;
+    a.group_add(&t);
+    b.group_sub(&t);
+}
+
+/// This performs permutation and butterfly arithmetic without twiddle for `[G]`
+fn self_swap_arithmetic<G: Group>(a: &mut [G], b: &mut [G], former: usize, latter: usize) {
+    let t = a[latter];
+    a[latter] = a[former];
+    a[former].group_add(&t);
+    a[latter].group_sub(&t);
+    let t = b[latter];
+    b[latter] = b[former];
+    b[former].group_add(&t);
+    b[latter].group_sub(&t);
+}
+
+/// This performs permutation and butterfly arithmetic twiddle for `[G]`
+fn self_swap_arithmetic_with_twiddle<G: Group>(
+    a: &mut [G],
+    b: &mut [G],
+    twiddle: &G::Scalar,
+    former: usize,
+    latter: usize,
+) {
+    let mut t = a[latter];
+    t.group_scale(twiddle);
+    a[latter] = a[former];
+    a[former].group_add(&t);
+    a[latter].group_sub(&t);
+    let mut t = b[latter];
+    t.group_scale(twiddle);
+    b[latter] = b[former];
+    b[former].group_add(&t);
+    b[latter].group_sub(&t);
+}
+
 #[cfg(test)]
 use rand_core::OsRng;
+
+#[cfg(test)]
+use crate::pasta::Fp;
 
 #[test]
 fn test_bitreverse() {
