81cb4026519c937604433e63cb5743e019204000
str4d <str4d@users.noreply.github.com>
2022-08-24 23:04:38 +0000

    deploy: 8fa116aa852c94ef2abcd1447e6ef5cf27ff8372

---
+++ rustdoc/latest/implementors/core/error/trait.Error.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"halo2_proofs":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"halo2_proofs/plonk/enum.Error.html\" title=\"enum halo2_proofs::plonk::Error\">Error</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
