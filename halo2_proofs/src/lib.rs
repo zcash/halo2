e8cb2e6470d86bc0d71990a909c80a96141baaab
str4d <jack@electriccoin.co>
2022-09-12 16:43:00 +0100

    halo2_proofs: Use qualified form of `rustdoc::broken_intra_doc_links`
    Co-authored-by: Daira Hopwood <daira@jacaranda.org>

--- halo2_proofs/src/lib.rs
+++ halo2_proofs/src/lib.rs
@@ -3,7 +3,7 @@
 #![cfg_attr(docsrs, feature(doc_cfg))]
 // The actual lints we want to disable.
 #![allow(clippy::op_ref, clippy::many_single_char_names)]
-#![deny(broken_intra_doc_links)]
+#![deny(rustdoc::broken_intra_doc_links)]
 #![deny(missing_debug_implementations)]
 #![deny(missing_docs)]
 #![deny(unsafe_code)]
