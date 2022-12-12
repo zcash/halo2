4a470fe2f23dd6d4c1b3c25820a720e70cdc43e6
CPerezz <c.perezbaro@gmail.com>
2022-12-12 15:37:00 +0100

    feat: Update emitter module to print Column annotations

--- halo2_proofs/src/dev/failure/emitter.rs
+++ halo2_proofs/src/dev/failure/emitter.rs
@@ -11,11 +11,24 @@ use crate::{
 
 fn padded(p: char, width: usize, text: &str) -> String {
     let pad = width - text.len();
+
     format!(
         "{}{}{}",
         iter::repeat(p).take(pad - pad / 2).collect::<String>(),
         text,
         iter::repeat(p).take(pad / 2).collect::<String>(),
+    )
+}
+
+fn column_type_and_idx(column: &metadata::Column) -> String {
+    format!(
+        "{}{}",
+        match column.column_type {
+            Any::Advice => "A",
+            Any::Fixed => "F",
+            Any::Instance => "I",
+        },
+        column.index
     )
 }
 
@@ -32,46 +45,83 @@ pub(super) fn render_cell_layout(
     highlight_row: impl Fn(Option<i32>, i32),
 ) {
     let col_width = |cells: usize| cells.to_string().len() + 3;
+    let mut col_headers = String::new();
 
     // If we are in a region, show rows at offsets relative to it. Otherwise, just show
     // the rotations directly.
     let offset = match location {
         FailureLocation::InRegion { region, offset } => {
-            eprintln!("{}Cell layout in region '{}':", prefix, region.name);
-            eprint!("{}  | Offset |", prefix);
+            col_headers
+                .push_str(format!("{}Cell layout in region '{}':\n", prefix, region.name).as_str());
+            col_headers.push_str(format!("{}  | Offset |", prefix).as_str());
             Some(*offset as i32)
         }
         FailureLocation::OutsideRegion { row } => {
-            eprintln!("{}Cell layout at row {}:", prefix, row);
-            eprint!("{}  |Rotation|", prefix);
+            col_headers.push_str(format!("{}Cell layout at row {}:\n", prefix, row).as_str());
+            col_headers.push_str(format!("{}  |Rotation|", prefix).as_str());
             None
         }
     };
+    eprint!("\n{}", col_headers);
 
-    // Print the assigned cells, and their region offset or rotation.
-    for (column, cells) in columns {
-        let width = col_width(*cells);
+    let widths: Vec<usize> = columns
+        .iter()
+        .map(|(col, _)| {
+            let size = match location {
+                FailureLocation::InRegion { region, offset } => {
+                    if let Some(column_ann) = region.column_annotations {
+                        if let Some(ann) = column_ann.get(&col) {
+                            ann.len()
+                        } else {
+                            col_width(column_type_and_idx(col).as_str().len())
+                        }
+                    } else {
+                        col_width(column_type_and_idx(col).as_str().len())
+                    }
+                }
+                FailureLocation::OutsideRegion { row } => {
+                    col_width(column_type_and_idx(col).as_str().len())
+                }
+            };
+            size
+        })
+        .collect();
+
+    // Print the assigned cells, and their region offset or rotation + the column name at which they're assigned to.
+    for ((column, cells), &width) in columns.iter().zip(widths.iter()) {
+        //let width = col_width(*cells);
         eprint!(
             "{}|",
             padded(
                 ' ',
                 width,
                 &format!(
-                    "{}{}",
-                    match column.column_type {
-                        Any::Advice => "A",
-                        Any::Fixed => "F",
-                        Any::Instance => "I",
-                    },
-                    column.index,
+                    "{}",
+                    match location {
+                        FailureLocation::InRegion { region, offset } => {
+                            if let Some(column_ann) = region.column_annotations {
+                                if let Some(ann) = column_ann.get(&column) {
+                                    format!("{}{}", ann.as_str(), "".to_string())
+                                } else {
+                                    column_type_and_idx(column)
+                                }
+                            } else {
+                                column_type_and_idx(column)
+                            }
+                        }
+                        FailureLocation::OutsideRegion { row } => {
+                            column_type_and_idx(column)
+                        }
+                    }
                 )
             )
         );
     }
+
     eprintln!();
     eprint!("{}  +--------+", prefix);
-    for cells in columns.values() {
-        eprint!("{}+", padded('-', col_width(*cells), ""));
+    for (cells, &width) in columns.values().zip(widths.iter()) {
+        eprint!("{}+", padded('-', width, ""));
     }
     eprintln!();
     for (rotation, row) in layout {
@@ -80,8 +130,8 @@ pub(super) fn render_cell_layout(
             prefix,
             padded(' ', 8, &(offset.unwrap_or(0) + rotation).to_string())
         );
-        for (col, cells) in columns {
-            let width = col_width(*cells);
+        for ((col, cells), &width) in columns.iter().zip(widths.iter()) {
+            //let width = col_width(*cells);
             eprint!(
                 "{}|",
                 padded(
